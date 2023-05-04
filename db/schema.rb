ef67a3dda7dcd1e49e4d385442e661bbd425ad4e
bitich <bitich777@yahoo.co.jp>
2023-05-04 15:45:47 +0900

    新規作成画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2023_05_04_040805) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
